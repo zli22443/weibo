c761a3cdb69f73f39c54cfa7e27a1b7ae5245abd
zli22443 <3477107094@qq.com>
2019-10-24 14:43:26 +0000

    命名路由

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,8 +1,8 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container">
-    <a href="/" class="navbar-brand">Weibo App</a>
+    <a href="{{route('home')}}" class="navbar-brand">Weibo App</a>
     <ul class="navbar-nav justify-content-end">
-      <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
+      <li class="nav-item"><a class="nav-link" href="{{route('help')}}">帮助</a></li>
       <li class="nav-item"><a class="nav-link" href="#">登录</a></li>
     </ul>
   </div>
