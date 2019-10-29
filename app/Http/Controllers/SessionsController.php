1203f4847f808435f803655d557ed637b7b8918a
Zli <3477107094@qq.com>
2019-10-29 21:50:08 +0800

    用户退出登录

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -27,6 +27,8 @@ class SessionsController extends Controller
     }
 
     public function destroy(){
-
+        Auth::logout();
+        session()->flash('success', '您已成功退出!');
+        return redirect('login');
     }
 }
