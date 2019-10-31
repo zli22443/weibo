8fc772b8a42b9cbd8fa0d784043c05a4ad09d1cb
Zli <3477107094@qq.com>
2019-10-31 22:56:43 +0800

    移除发邮件的form选项

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -72,12 +72,10 @@ class UsersController extends Controller
     public function sendEmailConfirmationTo($user){
         $views = 'emails.confirm';
         $data = compact('user');
-        $from = 'zli22443@gmail.com';
-        $name = $user->name;
         $to = $user->email;
         $subject ='感谢注册 Weibo 应用！请确认你的邮箱。';
-        Mail::send($views,$data,function ($message)use($from,$name,$to,$subject){
-            $message->from($from, $name)->to($to)->subject($subject);
+        Mail::send($views,$data,function ($message)use($to,$subject){
+            $message->to($to)->subject($subject);
         });
     }
 
