f8b5944608db6ec1382d1dae61f2ff09c3d7907a
Zli <3477107094@qq.com>
2019-10-31 20:19:07 +0800

    用户激活

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,6 +24,7 @@ $factory->define(User::class, function (Faker $faker) {
         'email_verified_at' => now(),
         'password' => bcrypt(123456), // password
         'remember_token' => Str::random(10),
+        'activated' => true,
         'created_at' => $date_time,
         'updated_at' => $date_time
     ];
