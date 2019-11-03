8d559d7e2b47a76d3c8a76fee567b95bd88b904d
Zli <3477107094@qq.com>
2019-11-03 22:53:41 +0800

    关注和粉丝列表页面

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -92,6 +92,18 @@ class UsersController extends Controller
         return redirect()->route('users.show',[$user]);
     }
 
+    public function followings(User $user){
+        $users = $user->followings()->paginate(20);
+        $title = $user->name.'关注的人';
+        return view('users.show_follow', compact('users', 'title'));
+    }
+
+    public function followers(User $user){
+        $users = $user->followers()->paginate(20);
+        $title = $user->name.'的粉丝';
+        return view('users.show_follow', compact('users', 'title'));
+    }
+
     public function destroy(User $user){
         $this->authorize('destroy',$user);
         $user->delete();
