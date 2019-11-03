0642553b014584908a2adc5a233dfab05cf0588b
Zli <3477107094@qq.com>
2019-11-03 13:06:18 +0800

    首页微博列表

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -8,6 +8,9 @@
           <section class="status-form">
             @include('statuses._status_form')
           </section>
+          <h4>微博列表</h4>
+          <hr>
+          @include('shared._feed')
         </div>
         <aside class="col-md-4">
           <section class="user-info">
